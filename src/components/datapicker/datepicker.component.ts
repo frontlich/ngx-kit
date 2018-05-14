955c6c897626848d6f0f31796ddfbeadc6d65077
frontlich <frontlich@frontlich.com>
2018-05-14 13:58:50 +0800

    to do datepicker

--- src/components/datapicker/datepicker.component.ts
+++ src/components/datapicker/datepicker.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '[nk-datepicker]',
-  templateUrl: './datepicker.component.html',
+  // templateUrl: './datepicker.component.html',
+  template: '',
   styleUrls: ['./datepicker.component.scss']
 })
 export class DatepickerComponent {
